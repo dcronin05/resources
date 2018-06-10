2735b92534a0940a85c9feba94516a8dc122ffa4
dcronin05 <37851794+dcronin05@users.noreply.github.com>
2018-06-10 14:10:04 -0600

    newGuessGame.py

--- files/python/guessGame.py
+++ files/python/guessGame.py
@@ -1,15 +1,35 @@
-# !/usr/bin/python3
-
-
 class Guess:
 
     def __init__(self):
-        self.guess = input("Please think of a number between 0 and 100!")
+        self.high = 100
+        self.low = 0
+        self.average()
+        self.hint()
+        self.bisect()
+
+    def average(self):
+        self.ans = (self.high + self.low) / 2
+
+    def bisect(self):
+        while True:
+            if self.input == 'c':
+                print("Game over. Your secret number was: %s" % self.ans)
+                exit()
+            elif self.input == 'h':
+                self.high = self.ans
+                self.average()
+                self.hint()
+            elif self.input == 'l':
+                self.low = self.ans
+                self.average()
+                self.hint()
+            else:
+                print("Sorry, I did not understand your input.")
+                self.hint()
+
+    def hint(self):
+        print("Is your secret number %s?" % round(self.ans))
+        self.input = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.\n")
 
 
-newGuess = Guess(input(""))
-
-newGuess.input_guess()
-
-print(newGuess.guess)
-# print(Guess.guess)
+guess = Guess()
