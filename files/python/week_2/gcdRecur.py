62e2fe9be98745e96833d6c585c0c603a4b2f514
Daniel <dcronin05@dcron.in>
2018-06-20 21:23:24 -0700

    from my phone

--- files/python/week_2/gcdRecur.py
+++ files/python/week_2/gcdRecur.py
@@ -1,5 +1,5 @@
 def gcdRecur(a, b):
 
-    return max(a, b) if a, b == 0 else gcdRecur(min(a, b), max(a, b) % min(a, b))
+    return max(a, b) if a and b == 0 else gcdRecur(min(a, b), max(a, b) % min(a, b))
 
-print(gcdRecur(42, 72))
+print(gcdRecur(555, 728949))
